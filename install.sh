e599966271cd1460a4df74eab3ece77acf659604
Enrico <enrico.goldmann@gmx.de>
2017-08-15 20:23:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 wget https://github.com/enno09/antergos_mediacenter/raw/master/packages.xml
 wget https://github.com/enno09/antergos_mediacenter/raw/master/antergos-mediacenter-setup.zip
 unzip antergos-mediacenter-setup.zip
