45c77e178465158d66082e4df955cfbfe7c2ccd8
Enrico <enrico.goldmann@gmx.de>
2017-08-15 20:19:26 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,5 @@
+wget https://github.com/enno09/antergos_mediacenter/raw/master/packages.xml
+wget https://github.com/enno09/antergos_mediacenter/raw/master/antergos-mediacenter-setup.zip
+unzip antergos-mediacenter-setup.zip
+cp antergos-mediacenter-setup /usr/share/antergos-openbox-setup
+pkexec /usr/share/cnchi/bin/cnchi -d -v -s bugsnag -p /home/antergos/packages.xml
