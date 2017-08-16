2e5642fd3d3fa4552f40ac79f41852e5b7c1e8cd
Enrico <enrico.goldmann@gmx.de>
2017-08-16 20:28:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ wget https://github.com/enno09/antergos_mediacenter/raw/master/packages.xml
 wget https://github.com/enno09/antergos_mediacenter/raw/master/antergos-mediacenter-setup.zip
 unzip antergos-mediacenter-setup.zip
 sudo mkdir -p /install/usr/share
+chmod 777 /install/usr/share
 cp -R antergos-mediacenter-setup /install/usr/share/antergos-openbox-setup
 pkexec /usr/share/cnchi/bin/cnchi -d -v -s bugsnag -p /home/antergos/packages.xml
